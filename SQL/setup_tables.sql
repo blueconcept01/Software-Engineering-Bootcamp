a6031c55074710b4aa326baebe56cb7cef490d78
Asha Singh <asha.singh7558@gmail.com>
2022-09-17 16:50:22 +0530

    Add files via upload

---
+++ SQL/setup_tables.sql
@@ -0,0 +1,172 @@
+-- create a database, named sqlfundamentals
+CREATE DATABASE sqlfundamentals;
+
+
+-- connect to the newly created database
+\c sqlfundamentals
+
+-- create table facilities
+CREATE TABLE facilities (
+    facid integer NOT NULL PRIMARY KEY,
+    name character varying(100) NOT NULL,
+    membercost numeric NOT NULL,
+    guestcost numeric NOT NULL,
+    initialoutlay numeric NOT NULL,
+    monthlymaintenance numeric NOT NULL
+);
+
+-- Populate table facilities
+INSERT INTO facilities (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) VALUES
+(0, 'Tennis Court 1', 5, 25, 10000, 200),
+(1, 'Tennis Court 2', 5, 25, 8000, 200),
+(2, 'Badminton Court', 0, 15.5, 4000, 50),
+(3, 'Table Tennis', 0, 5, 320, 10),
+(4, 'Massage Room 1', 35, 80, 4000, 3000),
+(5, 'Massage Room 2', 35, 80, 4000, 3000),
+(6, 'Squash Court', 3.5, 17.5, 5000, 80),
+(7, 'Snooker Table', 0, 5, 450, 15),
+(8, 'Pool Table', 0, 5, 400, 15);
+
+-- create table customer
+CREATE TABLE customers (
+  CustomerID INT NOT NULL PRIMARY KEY,
+  CustomerName VARCHAR DEFAULT NULL,
+  ContactName VARCHAR DEFAULT NULL,
+  Address VARCHAR DEFAULT NULL,
+  City VARCHAR DEFAULT NULL,
+  PostalCode VARCHAR DEFAULT NULL,
+  Country VARCHAR DEFAULT NULL
+);
+
+-- Populate table customer
+INSERT INTO customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES
+(1, 'Alfreds Futterkiste', 'Maria Anders', 'Obere Str. 57', 'Berlin', '12209', 'Germany'),
+(2, 'Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Avda. de la Constitución 2222', 'México D.F.', '05021', 'Mexico'),
+(3, 'Antonio Moreno Taquería', 'Antonio Moreno', 'Mataderos 2312', 'México D.F.', '05023', 'Mexico'),
+(4, 'Around the Horn', 'Thomas Hardy', '120 Hanover Sq.', 'London', 'WA1 1DP', 'UK'),
+(5, 'Berglunds snabbköp', 'Christina Berglund', 'Berguvsvägen 8', 'Luleå', 'S-958 22', 'Sweden'),
+(6, 'Blauer See Delikatessen', 'Hanna Moos', 'Forsterstr. 57', 'Mannheim', '68306', 'Germany'),
+(7, 'Blondel père et fils', 'Frédérique Citeaux', '24, place Kléber', 'Strasbourg', '67000', 'France'),
+(8, 'Bólido Comidas preparadas', 'Martín Sommer', 'C Araquil 67', 'Madrid', '28023', 'Spain'),
+(9, 'Bon app', 'Laurence Lebihans 12', 'rue des Bouchers', 'Marseille', '13008', 'France'),
+(10, 'Bottom-Dollar Marketse', 'Elizabeth Lincoln', '23 Tsawassen Blvd.', 'Tsawassen', 'T2F 8M4', 'Canada'),
+(11, 'B s Beverages', 'Victoria Ashworth', 'Fauntleroy Circus', 'London', 'EC2 5NT', 'UK'),
+(12, 'Cactus Comidas para llevar', 'Patricio Simpson', 'Cerrito 333', 'Buenos Aires', '1010', 'Argentina'),
+(13, 'Centro comercial Moctezuma', 'Francisco Chang', 'Sierras de Granada 9993', 'México D.F.', '05022', 'Mexico'),
+(14, 'Chop-suey Chinese', 'Yang Wang', 'Hauptstr. 29', 'Bern', '3012', 'Switzerland');
+
+-- Table structure for table `orders`
+
+CREATE TABLE orders (
+  OrderID INT NOT NULL PRIMARY KEY,
+  CustomerID INT DEFAULT NULL,
+  EmployeeID INT DEFAULT NULL,
+  OrderDate date DEFAULT NULL,
+  ShipperID INT DEFAULT NULL
+);
+
+INSERT INTO orders (OrderID, CustomerID, EmployeeID, OrderDate, ShipperID) VALUES
+(10248, 90, 5, '1996-07-04', 3),
+(10249, 81, 6, '1996-07-05', 1),
+(10250, 34, 4, '1996-07-08', 2),
+(10251, 84, 3, '1996-07-08', 1),
+(10252, 76, 4, '1996-07-09', 2),
+(10253, 34, 3, '1996-07-10', 2),
+(10254, 14, 5, '1996-07-11', 2),
+(10255, 68, 9, '1996-07-12', 3),
+(10256, 88, 3, '1996-07-15', 2),
+(10257, 35, 4, '1996-07-16', 3),
+(10258, 20, 1, '1996-07-17', 1),
+(10259, 13, 4, '1996-07-18', 3),
+(10260, 55, 4, '1996-07-19', 1),
+(10261, 61, 4, '1996-07-19', 2);
+
+--
+-- Table structure for table `shippers`
+--
+
+CREATE TABLE shippers (
+  ShipperID INT NOT NULL PRIMARY KEY,
+  ShipperName varchar DEFAULT NULL,
+  Phone varchar DEFAULT NULL
+);
+
+--
+-- Dumping data for table `shippers`
+--
+
+INSERT INTO shippers (ShipperID, ShipperName, Phone) VALUES
+(1, 'Speedy Express', '(503) 555-9831'),
+(2, 'United Package', '(503) 555-3199'),
+(3, 'Federal Shipping', '(503) 555-9931');
+
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE orders
+  ADD CONSTRAINT orders_ibfk_1 FOREIGN KEY (CustomerID) REFERENCES customers (CustomerID),
+  ADD CONSTRAINT orders_ibfk_2 FOREIGN KEY (ShipperID) REFERENCES shippers (ShipperID);
+
+
+-- create table employees
+
+CREATE TABLE employees (
+EMPLOYEE_ID INTEGER PRIMARY KEY,
+FIRST_NAME VARCHAR,
+LAST_NAME VARCHAR,
+EMAIL VARCHAR,
+PHONE_NUMBER VARCHAR,
+HIRE_DATE DATE,
+JOB_ID VARCHAR,
+SALARY DECIMAL(10, 2),
+COMMISSION_PCT DECIMAL(10, 2),
+MANAGER_ID INTEGER,
+DEPARTMENT_ID INTEGER
+);
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO employees (EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, COMMISSION_PCT, MANAGER_ID, DEPARTMENT_ID) VALUES
+(100, 'Steven', 'King', 'SKING', '515.123.4567', '1987-06-17', 'AD_PRES', 24000.00, 0.00, 100, 90),
+(101, 'Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', '1987-06-18', 'AD_VP', 17000.00, 0.00, 100, 90),
+(102, 'Lex', 'De Haan', 'LDEHAAN', '515.123.4569', '1987-06-19', 'AD_VP', 17000.00, 0.00, 100, 90),
+(103, 'Alexander', 'Hunold', 'AHUNOLD', '590.423.4567', '1987-06-20', 'IT_PROG', 9000.00, 0.00, 102, 60),
+(104, 'Bruce', 'Ernst', 'BERNST', '590.423.4568', '1987-06-21', 'IT_PROG', 6000.00, 0.00, 103, 60),
+(105, 'David', 'Austin', 'DAUSTIN', '590.423.4569', '1987-06-22', 'IT_PROG', 4800.00, 0.00, 103, 60),
+(106, 'Valli', 'Pataballa', 'VPATABAL', '590.423.4560', '1987-06-23', 'IT_PROG', 4800.00, 0.00, 103, 60),
+(107, 'Diana', 'Lorentz', 'DLORENTZ', '590.423.5567', '1987-06-24', 'IT_PROG', 4200.00, 0.00, 103, 60),
+(108, 'Nancy', 'Greenberg', 'NGREENBE', '515.124.4569', '1987-06-25', 'FI_MGR', 12000.00, 0.00, 101, 100),
+(109, 'Daniel', 'Faviet', 'DFAVIET', '515.124.4169', '1987-06-26', 'FI_ACCOUNT', 9000.00, 0.00, 108, 100),
+(110, 'John', 'Chen', 'JCHEN', '515.124.4269', '1987-06-27', 'FI_ACCOUNT', 8200.00, 0.00, 108, 100);
+
+-- create table departments
+
+CREATE TABLE departments (
+DEPARTMENT_ID INTEGER PRIMARY KEY, 
+DEPARTMENT_NAME VARCHAR, 
+MANAGER_ID INTEGER, 
+LOCATION_ID INTEGER
+);
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT into departments (DEPARTMENT_ID, DEPARTMENT_NAME, MANAGER_ID, LOCATION_ID) VALUES
+(10, 'Administration', 100, 1700),
+(20, 'Marketing', 103, 1800),
+(30, 'Purchasing', 102, 1700),
+(40, 'Human Resources', 108, 2400),
+(50, 'Shipping', 103, 1500),
+(60, 'IT', 103, 1400),
+(90, 'Executive', 100, 1700),
+(100, 'Finance', 108, 1700 );
+
+--
+-- Adding foreign keys
+--
+
+ALTER TABLE employees ADD CONSTRAINT department_fkn FOREIGN KEY (DEPARTMENT_ID) REFERENCES departments (DEPARTMENT_ID);
