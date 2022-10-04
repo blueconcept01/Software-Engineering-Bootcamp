54be7e7bdd65f2d32317f450e01aae13fee69baf
Asha Singh <asha.singh7558@gmail.com>
2022-10-04 19:40:05 +0530

    Update s3-list-all-objects.py

--- S3/s3-list-all-objects.py
+++ S3/s3-list-all-objects.py
@@ -7,7 +7,9 @@ session = boto3.Session(aws_access_key_id='<access-key>', aws_secret_access_key=
 ## list all objects from bucket
 s3 = session.resource('s3')
 
+## access the particular bucket
 my_bucket = s3.Bucket('<bucket-name>')
 
+## list all the objects from the bucket
 for my_bucket_object in my_bucket.objects.all():
     print(my_bucket_object.key)
