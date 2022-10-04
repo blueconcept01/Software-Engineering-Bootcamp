5a37f32f97e2200bd499886e5b35f9324d2c6095
Asha Singh <asha.singh7558@gmail.com>
2022-10-04 19:39:23 +0530

    Update s3-delete-bucket.py

--- S3/s3-delete-bucket.py
+++ S3/s3-delete-bucket.py
@@ -4,11 +4,13 @@ import boto3
 ## add credentials to this line
 session = boto3.Session(aws_access_key_id='<access-key>', aws_secret_access_key='<secret-key>')
 
+## get the connection the S3 resource
 s3 = session.resource('s3')
 
-## connect with the bucket
+## connect with the bucket; this will setup the connectivity to the particular bucket; after you can perform the operations
 bucket = s3.Bucket('<bucket-name>')
 
+## you cannot delete a bucket if there are any objects exists.
 ## empty the bucket
 bucket.objects.all().delete()
 
