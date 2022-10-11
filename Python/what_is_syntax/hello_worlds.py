d04edf1eff6c1c7ecdbd59cdbcf2f06929340a61
blueconcept01 <31360801+blueconcept01@users.noreply.github.com>
2022-10-10 17:37:00 -0700

    Added Lesson 4

---
+++ Python/what_is_syntax/hello_worlds.py
@@ -0,0 +1,10 @@
+print("helloworld!") # this print() will send out a message on the terminal based on the characters in between the ""
+
+# these empty lines are done on purpose
+# anything after the # are comments and do not run as code, they are for humans like you and me to read 
+
+print("this is command 2")
+print("this is command 3")
+print("My name is")
+
+print("Jack Tran") # Replace with your name
