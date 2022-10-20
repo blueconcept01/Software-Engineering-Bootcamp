3c8ab8a632e88e00646f10b007b41221e52c1030
Asha Singh <asha.singh7558@gmail.com>
2022-10-21 01:42:23 +0530

    Update for_loops.py

--- Python/unit_2/for_loops/for_loops.py
+++ Python/unit_2/for_loops/for_loops.py
@@ -137,70 +137,70 @@ class TestDataTypesAndVariables(unittest.TestCase):
     def test_functions(self):
 
         # list_numbers function
-        self.assertEqual(list_numbers(5), [0, 1, 2, 3, 4, 5], msg="It should return the list from 0 till given number") 
-        self.assertEqual(list_numbers(-1), -1, msg="It should return -1 as the given number is negative") 
+        self.assertEqual(list_numbers(5), [0, 1, 2, 3, 4, 5], msg="It should return the list i.e [0, 1, 2, 3, 4, 5] for the given number 5.") 
+        self.assertEqual(list_numbers(-2), -1, msg="It should return -1 as the given number -2 is negative") 
         
         # even_numbers function
-        self.assertEqual(even_numbers(5), [2, 4], msg="It should return list of even numbers from 1 till the given number")
-        self.assertEqual(even_numbers(12), [2, 4, 6, 8, 10, 12], msg="It should return list of even numbers from 1 till the given number")
-        self.assertEqual(even_numbers(1), [], msg="It should return empty list")
+        self.assertEqual(even_numbers(5), [2, 4], msg="It should return list of even numbers from 1 till the given number i.e for number 5 it should return [2, 4]")
+        self.assertEqual(even_numbers(12), [2, 4, 6, 8, 10, 12], msg="It should return list of even numbers from 1 till the given number i.e for number 12 it should return [2, 4, 6, 8, 10, 12]")
+        self.assertEqual(even_numbers(1), [], msg="It should return empty list as the given list is empty []")
 
         # odd_numbers function
-        self.assertEqual(odd_numbers(5), [1, 3, 5], msg="It should return list of odd numbers from 1 till the given number")
-        self.assertEqual(odd_numbers(12), [1, 3, 5, 7, 9, 11], msg="It should return list of odd numbers from 1 till the given number")
-        self.assertEqual(odd_numbers(0), [], msg="It should return empty list")       
+        self.assertEqual(odd_numbers(5), [1, 3, 5], msg="It should return list of odd numbers from 1 till the given number i.e for number 5 it should return [1, 3, 5]")
+        self.assertEqual(odd_numbers(12), [1, 3, 5, 7, 9, 11], msg="It should return list of odd numbers from 1 till the given number i.e for number 12 it should return [1, 3, 5, 7, 9, 11]")
+        self.assertEqual(odd_numbers(0), [], msg="It should return empty list as the given list is empty []")       
 
         # individual_characters function
-        self.assertEqual(individual_characters("hello"), ['h', 'e', 'l', 'l', 'o'], msg="It should return list of characters in the string")
-        self.assertEqual(individual_characters("okay"), ['o', 'k', 'a', 'y'], msg="It should return list of characters in the string")
+        self.assertEqual(individual_characters("hello"), ['h', 'e', 'l', 'l', 'o'], msg="It should return list of characters in the string i.e for string 'hello' it should return ['h', 'e', 'l', 'l', 'o']")
+        self.assertEqual(individual_characters("okay"), ['o', 'k', 'a', 'y'], msg="It should return list of characters in the string i.e for string 'okay' it should return ['o', 'k', 'a', 'y']")
         self.assertEqual(individual_characters(""), -1, msg="It should return -1 as given string is empty")    
 
         # individual_unique_characters function
-        self.assertEqual(individual_unique_characters("hello"), ['h', 'e', 'l', 'o'], msg="It should return list of unique characters in the string")
-        self.assertEqual(individual_unique_characters("okay"), ['o', 'k', 'a', 'y'], msg="It should return list of unique characters in the string")
+        self.assertEqual(individual_unique_characters("hello"), ['h', 'e', 'l', 'o'], msg="It should return list of unique characters in the string i.e for string 'hello' it should return ['h', 'e', 'l', 'o']")
+        self.assertEqual(individual_unique_characters("okay"), ['o', 'k', 'a', 'y'], msg="It should return list of unique characters in the string i.e for string 'okay' it should return ['o', 'k', 'a', 'y']")
         self.assertEqual(individual_characters(""), -1, msg="It should return -1 as given string is empty")               
 
         # sum_list function
-        self.assertEqual(sum_list([1, 2, 3, 4]),10, msg="It should return the sum of all the numbers in the list")
-        self.assertEqual(sum_list([2, 2, 2]),6, msg="It should return the sum of all the numbers in the list")
+        self.assertEqual(sum_list([1, 2, 3, 4]),10, msg="It should return the sum of all the numbers in the list i.e for list [1, 2, 3, 4] it should return 1+2+3+4=10")
+        self.assertEqual(sum_list([2, 2, 2]),6, msg="It should return the sum of all the numbers in the list i.e for list [2, 2, 2] it should return 2+2+2=6")
         self.assertEqual(sum_list([]), -1, msg="It should return -1 as given list is empty") 
 
         # max_list function
-        self.assertEqual(max_list([1, 2, 3, 4]),4, msg="It should return the maximum of all the numbers in the list")
-        self.assertEqual(max_list([2, 2, 2]),2, msg="It should return the maximum of all the numbers in the list")
+        self.assertEqual(max_list([1, 2, 3, 4]),4, msg="It should return the maximum of all the numbers in the list i.e for list [1, 2, 3, 4] it should return 4")
+        self.assertEqual(max_list([2, 2, 2]),2, msg="It should return the maximum of all the numbers in the list i.e for list [2, 2, 2] it should return 2")
         self.assertEqual(max_list([]), -1, msg="It should return -1 as given list is empty")
 
         # min_list function
-        self.assertEqual(min_list([1, 2, 3, 4]),1, msg="It should return the minimum of all the numbers in the list")
-        self.assertEqual(min_list([2, 2, 2]),2, msg="It should return the minimum of all the numbers in the list")
+        self.assertEqual(min_list([1, 2, 3, 4]),1, msg="It should return the minimum of all the numbers in the list i.e for list [1, 2, 3, 4] it should return 1")
+        self.assertEqual(min_list([2, 2, 2]),2, msg="It should return the minimum of all the numbers in the list i.e for list [2, 2, 2] it should return 2")
         self.assertEqual(min_list([]), -1, msg="It should return -1 as given list is empty")        
 
         # find_num_in_list function
-        self.assertEqual(find_num_in_list([1, 2, 3, 4], 4),"Found", msg="It should return Found as number exists in the list")
-        self.assertEqual(find_num_in_list([2, 2, 2], 3),"Not Found", msg="It should return Not Found as the number not found")
+        self.assertEqual(find_num_in_list([1, 2, 3, 4], 4),"Found", msg="It should return Found as number exists in the list i.e for list [1, 2, 3, 4] and number 4 it should return 'Found' as 4 is present in the list")
+        self.assertEqual(find_num_in_list([2, 2, 2], 3),"Not Found", msg="It should return Not Found as the number not found i.e for list [2, 2, 2] and number 3 it should return 'Not Found' as 3 is not present")
         self.assertEqual(find_num_in_list([], 5), -1, msg="It should return -1 as given list is empty")
 
         # get_positives function
-        self.assertEqual(get_positives([1, -2, 3, -1, -8, 0]),[1, 3], msg="It should return the list of positive numbers that exists in the list")
-        self.assertEqual(get_positives([-1, -2, -3]),-1, msg="It should return -1 as there are no positive numbers in the list")
+        self.assertEqual(get_positives([1, -2, 3, -1, -8, 0]),[1, 3], msg="It should return the list of positive numbers that exists in the list i.e for list [1, -2, 3, -1, -8, 0] it should return [1, 3]")
+        self.assertEqual(get_positives([-1, -2, -3]),-1, msg="It should return -1 as there are no positive numbers in the list i.e for list [-1, -2, -3] it should return -1 as there are no positive integer.")
         self.assertEqual(get_positives([]), -1, msg="It should return -1 as given list is empty")     
 
         # get_negative function
-        self.assertEqual(get_negative([1, -2, 3, -1, -8, 0]),[-2, -1, -8], msg="It should return the list of negative numbers that exists in the list")
-        self.assertEqual(get_negative([1, 2, 3]),-1, msg="It should return -1 as there are no negative numbers in the list")
+        self.assertEqual(get_negative([1, -2, 3, -1, -8, 0]),[-2, -1, -8], msg="It should return the list of negative numbers that exists in the list i.e for list [1, -2, 3, -1, -8, 0] it should return [-2, -1, -8]")
+        self.assertEqual(get_negative([1, 2, 3]),-1, msg="It should return -1 as there are no negative numbers in the list i.e for list [1, 2, 3] it should return -1 as there are no negative integer.")
         self.assertEqual(get_negative([]), -1, msg="It should return -1 as given list is empty")    
 
         # get_keys function
-        self.assertEqual(get_keys({'hello': 1, 'my': 7, 'okay': 4}),['hello', 'my', 'okay'], msg="It should return the keys from the given dictionary")
+        self.assertEqual(get_keys({'hello': 1, 'my': 7, 'okay': 4}),['hello', 'my', 'okay'], msg="It should return the keys from the given dictionary i.e for dictionary {'hello': 1, 'my': 7, 'okay': 4} it should return ['hello', 'my', 'okay']")
         self.assertEqual(get_keys({}), -1, msg="It should return -1 as given dictionary is empty")   
 
         # get_values function
-        self.assertEqual(get_values({'hello': 1, 'my': 7, 'okay': 4}),[1, 7, 4], msg="It should return the values from the given dictionary")
+        self.assertEqual(get_values({'hello': 1, 'my': 7, 'okay': 4}),[1, 7, 4], msg="It should return the values from the given dictionary i.e for dictionary {'hello': 1, 'my': 7, 'okay': 4} it should return [1, 7, 4]")
         self.assertEqual(get_values({}), -1, msg="It should return -1 as given dictionary is empty")          
 
         # get_coordinates function
-        self.assertEqual(get_coordinates([1, 2], [3, 4]),[(1, 3), (1, 4), (2, 3), (2, 4)], msg="It should return the list of (x, y) coordinates)")
-        self.assertEqual(get_coordinates([1], [3, 4]),[(1, 3), (1, 4)], msg="It should return the list of (x, y) coordinates)")
+        self.assertEqual(get_coordinates([1, 2], [3, 4]),[(1, 3), (1, 4), (2, 3), (2, 4)], msg="It should return the list of all possible (x, y) coordinates)")
+        self.assertEqual(get_coordinates([1], [3, 4]),[(1, 3), (1, 4)], msg="It should return the list of all possible (x, y) coordinates)")
         self.assertEqual(get_coordinates([], [1, 2]), -1, msg="It should return -1 as x-coordinates is empty")                      
         self.assertEqual(get_coordinates([1, 2], []), -1, msg="It should return -1 as y-coordinates is empty")          
 
